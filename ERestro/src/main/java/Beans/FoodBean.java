dd8dd14c0f28f97faa0e447016e50204dbe13488
bluebirrd <anna.kurilo@outlook.com>
2014-11-09 14:21:22 +0200

    added functionality to FoodBean

--- ERestro/src/main/java/Beans/FoodBean.java
+++ ERestro/src/main/java/Beans/FoodBean.java
@@ -2,13 +2,40 @@ package Beans;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.inject.Named;
+
+import org.springframework.context.annotation.Scope;
 
 import com.bionic.edu.ERestro.Food;
 
 @ManagedBean(name="food", eager = true)
 @RequestScoped
+/*
+ * @Named
+ *	@Scope("request")
+ */
 public class FoodBean {
 	private Food dish;
+	/*
+	 * @Inject FoodBeanService foodService;
+	 */
+
+	public Food getDish() {
+		return dish;
+	}
+
+	public void setDish(Food dish) {
+		this.dish = dish;
+	}
 	
+	public String saveFood() {
+		//foodService.save();
+		return "editMenu";
+	}
+	
+	public String editFood(String id) {
+		//dish = foodService.findById();
+		return "newDish";
+	}
 	
 }
